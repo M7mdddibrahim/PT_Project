cc6017764e8c672eb6fa83dbb2514223891a7919
nadabdallah <nadabdallah25@gmail.com>
2023-05-08 00:15:39 +0300

    phase 2

---
+++ Phase1_Code/Phase2_Code/CellPosition.cpp
@@ -0,0 +1,142 @@
+#include "CellPosition.h"
+#include "UI_Info.h"
+
+CellPosition::CellPosition () 
+{
+	// (-1) indicating an invalid cell (uninitialized by the user)
+	vCell = -1; 
+	hCell = -1; 
+}
+
+CellPosition::CellPosition (int v, int h)
+{ 
+	// (-1) indicating an invalid cell (uninitialized by the user)
+	vCell = -1; 
+	hCell = -1; 
+
+	SetVCell(v);
+	SetHCell(h);
+}
+
+CellPosition::CellPosition (int cellNum)
+{
+	(*this) = GetCellPositionFromNum(cellNum); // the function call with build a cell position (vCell and hCell)
+												// from the passed (cellNum)
+												// (*this) = ... --> this will copy the returned (vCell and hCell)
+												//                   to the data members (vCell and hCell)
+}
+
+bool CellPosition::SetVCell(int v) 
+{
+	///TODO: Implement this function as described in the .h file (don't forget the validation)
+	if (v > 9 || v < 0)
+	{
+		return false;
+	}
+	else {
+		this->vCell = v;
+		return true;
+	}
+
+	return false; // this line sould be changed with your implementation
+}
+
+bool CellPosition::SetHCell(int h) 
+{
+	///TODO: Implement this function as described in the .h file (don't forget the validation)
+	if (h > 10 || h < 0)
+	{
+		return false;
+	}
+	else {
+		this->hCell = h;
+		return true;
+	}
+
+	return false; // this line sould be changed with your implementation
+}
+
+int CellPosition::VCell() const 
+{
+	return vCell;
+}
+
+int CellPosition::HCell() const 
+{
+	return hCell;
+}
+
+bool CellPosition::IsValidCell() const 
+{
+	///TODO: Implement this function as described in the .h file
+	if ((vCell <= 8 && vCell >= 0) && (hCell >= 0 || hCell <= 10))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+	return false; // this line sould be changed with your implementation
+}
+
+int CellPosition::GetCellNum() const
+{
+	return GetCellNumFromPosition(*this); // (*this) is the calling object of GetCellNum
+										  // which means the object of the current data members (vCell and hCell)
+}
+
+int CellPosition::GetCellNumFromPosition(const CellPosition & cellPosition)
+{
+	// Note:
+	// this is a static function (do NOT need a calling object so CANNOT use the data members of the calling object, vCell&hCell)
+	// just define an integer that represents cell number and calculate it using the passed cellPosition then return it
+
+	///TODO: Implement this function as described in the .h file
+
+	
+
+	return 89 - (cellPosition.vCell * 11) + cellPosition.hCell; // this line should be changed with your implementation
+}
+
+CellPosition CellPosition::GetCellPositionFromNum(int cellNum)
+{
+	// this is a static function (do NOT need a calling object so CANNOT use the data members of the calling object, vCell&hCell)
+
+	CellPosition position;
+
+	/// TODO: Implement this function as described in the .h file
+	for (int i = 0; i < NumVerticalCells; i++)
+	{
+		for (int j = 0; j < NumHorizontalCells; j++)
+		{
+			if ((89 - (i * NumHorizontalCells) + j) == cellNum)
+			{
+				position.SetVCell(i);
+				position.SetHCell(j);
+				break;
+			}
+		}
+	}
+
+	// Note: use the passed cellNum to set the vCell and hCell of the "position" variable declared inside the function
+	//       I mean: position.SetVCell(...) and position.SetHCell(...) then return it
+
+
+	return position;
+}
+
+void CellPosition::AddCellNum (int addedNum)
+{
+	
+	/// TODO: Implement this function as described in the .h file
+	int newcell = GetCellNum() + addedNum;
+	CellPosition newpos;
+	newpos.GetCellPositionFromNum(newcell);
+	SetVCell(newpos.VCell());
+	SetHCell(newpos.HCell());
+
+	// Note: this function updates the data members (vCell and hCell) of the calling object
+
+}
