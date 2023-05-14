8b6b85d88a07e8663f7e19887d090f2d5797c7e8
M7mdddibrahim <m7mdalhussini@gmail.com>
2023-05-14 12:07:47 +0300

    ..

--- Phase2_Code/CardFive.cpp
+++ Phase2_Code/CardFive.cpp
@@ -1,5 +1,6 @@
 #include "CardFive.h"
-
+#include "Ladder.h"
+#include "Snake.h"
 
 
 CardFive::CardFive(const CellPosition& pos) : Card(pos) //Set the Cell Position of the Card
@@ -24,10 +25,27 @@ void CardFive::Apply(Grid* pGrid, Player* pPlayer)
 	Card::Apply(pGrid, pPlayer);
 	Output* pOut = pGrid->GetOutput();
 	Input* pIn = pGrid->GetInput();
+	Ladder* NextLadder = pGrid->GetNextLadder(position.GetCellNum());
+	Snake* NextSnake = pGrid->GetNextSnake(position.GetCellNum());
+	
 	pOut->PrintMessage("Move Backword the same number of steps that you just rolled. Click to continue...");
 	int x, y;
 	pIn->GetPointClicked(x, y);
 	pOut->ClearStatusBar();
 	pPlayer->Move(pGrid, (pPlayer->getjustRolledDiceNum())*-1);
 
+	if (position.GetCellNum() == NextLadder->GetEndPosition().GetCellNum())
+	{
+		pOut->PrintMessage("Now you will move up the ladder. Click to continue...");
+		int x, y;
+		pIn->GetPointClicked(x, y);
+		pOut->ClearStatusBar();
+	}
+	else if (position.GetCellNum() == NextSnake->GetEndPosition().GetCellNum())
+	{
+		pOut->PrintMessage("Now you will move down the Snake. Click to continue...");
+		int x, y;
+		pIn->GetPointClicked(x, y);
+		pOut->ClearStatusBar();
+	}
 }
